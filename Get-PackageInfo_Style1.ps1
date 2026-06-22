53e505a9ddd6c489487d659559d76f9304bee83d
unknown <Administrator@corp.pcxlab.com>
2026-06-22 23:23:47 +0530

    Pakcge info style1 and style2 added

---
+++ Get-PackageInfo_Style1.ps1
@@ -0,0 +1,49 @@
+﻿function Get-PackageInfo02 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$Path
+    )
+
+    # Normalize path
+    # $resolvedPath = (Resolve-Path -Path $Path -ErrorAction Stop).Path
+    $resolvedPath = (Resolve-Path -Path $Path -ErrorAction Stop).ProviderPath
+
+    # Split path
+    $pathSplit = $resolvedPath -split "\\"
+
+    $packageName = $pathSplit[-1]
+    $product     = $pathSplit[-2]
+    $company     = $pathSplit[-3]
+
+    # Extract version
+    $versionSplit = $packageName -split "_"
+
+    if ($versionSplit.Count -gt 1) {
+        $version = $versionSplit[-1]
+    } else {
+        $version = $null
+    }
+
+    $PackageInfo = [PSCustomObject]@{
+        PackageName = $packageName
+        Company     = $company
+        Product     = $product
+        Version     = $version
+        FullPath    = $resolvedPath
+    }
+
+    return $PackageInfo
+}
+
+<#
+Usage Example
+$result = Get-PackageInfo -Path "\\192.168.25.214\Package_Source\Applications\Igor_Pavlov\7Zip\7Zip_26.0.0.0"
+
+$result
+$result.Company
+#>
+
+$result = Get-PackageInfo02 -Path "\\192.168.25.214\Package_Source\Applications\Igor_Pavlov\7zip\7zip_26.0.0"
+
+$result
---
+++ Get-PackageInfo_Style2.ps1
@@ -0,0 +1,47 @@
+﻿function Get-PackageInfo01 {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]$Path
+    )
+
+    # Normalize path
+    #$resolvedPath = Resolve-Path -Path $Path -ErrorAction Stop
+    $resolvedPath = (Resolve-Path -Path $Path -ErrorAction Stop).ProviderPath
+
+
+    # Extract parts using built-in cmdlets
+    $packageName = Split-Path -Path $resolvedPath -Leaf
+    $product     = Split-Path -Path (Split-Path -Path $resolvedPath -Parent) -Leaf
+    $company     = Split-Path -Path (Split-Path -Path (Split-Path -Path $resolvedPath -Parent) -Parent) -Leaf
+
+    # Extract version safely
+    $version = if ($packageName -match '_(.+)$') {
+        $Matches[1]
+    } else {
+        $null
+    }
+
+   $PackageInfo = [PSCustomObject]@{
+        PackageName = $packageName
+        Company     = $company
+        Product     = $product
+        Version     = $version
+        FullPath    = $resolvedPath
+
+    }
+        return $PackageInfo
+
+}
+
+$result = Get-PackageInfo01 -Path "\\192.168.25.214\Package_Source\Applications\Igor_Pavlov\7zip\7zip_26.0.0"
+
+$result
+
+<#
+Usage Exanple
+$result = Get-PackageInfo -Path "\\192.168.25.214\Package_Source\Applications\Igor_Pavlov\7Zip\7Zip_26.0.0.0"
+
+$result
+$result.Company
+#>
